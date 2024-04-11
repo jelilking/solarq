da27ab2878afd665303c024e3e0345036ad7b495
jelilking <abduljelilozigi@gmail.com>
2024-04-11 10:46:34 +0100

    finished lp

--- sandbox.js
+++ sandbox.js
@@ -1,34 +1,74 @@
-const close = document.querySelectorAll("#close");
-const poppupEl = document.querySelector(".popup");
-const poppupEl1 = document.querySelector(".popup1");
+// const close = document.querySelectorAll("#close");
+// const poppupEl = document.querySelector(".popup");
+// const poppupEl1 = document.querySelector(".popup1");
 
-window.addEventListener("load", () => {
-  setTimeout(function open(event) {
-    poppupEl1.style.display = "block";
-  }, 8000);
-});
+// window.addEventListener("load", () => {
+//   setTimeout(function open(event) {
+//     poppupEl1.style.display = "block";
+//   }, 8000);
+// });
 
-window.addEventListener("load", () => {
-  setTimeout(function open(event) {
-    poppupEl1.style.display = "block";
-  }, 32000);
-});
+// window.addEventListener("load", () => {
+//   setTimeout(function open(event) {
+//     poppupEl.style.display = "block";
+//   }, 32000);
+// });
 
-window.addEventListener("load", () => {
-  setTimeout(function open(event) {
-    poppupEl.style.display = "block";
-  }, 12000);
-});
+// window.addEventListener("load", () => {
+//   setTimeout(function open(event) {
+//     poppupEl1.style.display = "block";
+//   }, 12000);
+// });
 
-window.addEventListener("load", () => {
-  setTimeout(function open(event) {
-    poppupEl1.style.display = "block";
-  }, 46000);
-});
+// window.addEventListener("load", () => {
+//   setTimeout(function open(event) {
+//     poppupEl.style.display = "block";
+//   }, 46000);
+// });
 
-close.forEach((clo) => {
-  clo.addEventListener("click", () => {
-    poppupEl1.style.display = "none";
-    poppupEl.style.display = "none";
-  });
-});
+// close.forEach((clo) => {
+//   clo.addEventListener("click", () => {
+//     poppupEl1.style.display = "none";
+//     poppupEl.style.display = "none";
+//   });
+// });
+
+// function hidePopup() {
+//   poppupEl1.style.display = "none";
+//   poppupEl.style.display = "none";
+// }
+
+// setTimeout(hidePopup, 4000);
+
+// const closeButtons = document.querySelectorAll("#close");
+// const popupEl = document.querySelector(".popup");
+// const popupEl1 = document.querySelector(".popup1");
+
+// window.addEventListener("load", () => {
+//   setTimeout(() => {
+//     popupEl1.style.display = "block";
+//   }, 8000);
+
+//   setTimeout(() => {
+//     popupEl.style.display = "block";
+//   }, 32000);
+
+//   setTimeout(() => {
+//     popupEl1.style.display = "block";
+//   }, 12000);
+
+//   setTimeout(() => {
+//     popupEl.style.display = "block";
+//   }, 46000);
+// });
+
+// function hidePopup() {
+//   popupEl1.style.display = "none";
+//   popupEl.style.display = "none";
+// }
+
+// closeButtons.forEach((clo) => {
+//   clo.addEventListener("click", hidePopup);
+// });
+
+// setTimeout(hidePopup, 4000);
